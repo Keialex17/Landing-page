3d23dba21610b9a73c8aa0636da188af260ac38c
Keialex17 <hernandezkeiber34@gmail.com>
2022-11-22 03:49:23 +0000

    pocoapoco

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,15 @@
 import React, {Fragment} from "react";
 import Navbar from "./Navbar";
 import Card from "./Card";
+import Jumbotron from "./Jumbotron";
 
 const Home=()=>{
 	return(
 		<Fragment>
 			<Navbar/>
+			<Jumbotron/>
+			<Card/>
+			<Card/>
 			<Card/>
 		</Fragment>
 	)
